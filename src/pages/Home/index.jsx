be4e6d11e58601dce8bec03418c1550fad38560b
FunkSwe <funkcampswe@gmail.com>
2024-10-20 13:46:33 +0200

    some text animation added to use

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,6 +8,19 @@ import IMG4 from '@/assets/images/2018_panel.jpg';
 import Lenis from '@studio-freight/lenis';
 import PageTransition from '@/components/PageTransition';
 import TeachersList from '@/components/teachers/TeachersList';
+import { AnimatedText } from '@/components/animations/AnimatedText';
+
+const firstText =
+  "Next year marks the 20th anniversary of Funkcamp. The goal has always been to connect the pioneers of locking with the younger generation. When I started, it was about learning from those who lived the dance, the culture, all of it. At the same time, I wanted to give the youth in Sweden the chance to gain the same knowledge, to raise the level of dancers here by inviting pioneers to teach and build connections between generations. It's been an incredible experience, and next year will also be the last Funkcamp. What the future holds, no one knows, but the young generation needs to pick up the torch, respect the old, and push the scene forward.";
+
+const secondText =
+  "Funkcamp started in 2005, as the first camp in Sweden and Europe dedicated to locking. The journey began with my involvement in a kids' show where I wanted to introduce street styles, and from there, the idea of Funkcamp was born, with the help of OG Skeeter Rabbit and Greg Campbellock Jr. We set the plans in motion to create a space for the next generation to learn directly from the source.";
+
+const fourtText =
+  "Over the past 20 years, the camp has been a bridge for many younger dancers, igniting their passion. Without the support of OG Skeeter Rabbit, TONY GOGO, and Greg Campbellock Jr (R.I.P), Funkcamp would not have existed. I'm deeply honored by their continued support and the brotherhood we've built. Special thanks go to Manne Schutt, Amine Baraka, and Willow for their vital roles in making the camp a success";
+
+const fifthText =
+  "The past two decades have been remarkable, and we've been lucky to have teachers like Damon Frost, Patrik \"PRIME\" Helge, Loose Caboose, Lock Smith, and Anthony Edwards share their passion with us. The camp's legacy now lives on under the roof of Fryshuset, where we've hosted the past few camps. We look forward to celebrating the 20th anniversary in 2025, with more great guests and unforgettable moments.";
 
 const Home = () => {
   useEffect(() => {
@@ -22,16 +35,18 @@ const Home = () => {
   }, []);
 
   return (
-    <main className='w-screen h-full'>
+    <main className='w-screen h-full p-10'>
       {/*  <Hero /> */}
       <Hero />
+      <section className='section'>
+        <AnimatedText text='Welcome!' className='title' />
+      </section>
       <div id='teachers'>
         <TeachersList />
       </div>
-      {/*   <img src={IMG} alt="" /> */}
       {/* Teacher presentation goes here */}
       <Section
-        image={'/src/assets/images/asa2022.jpeg'}
+        image={IMG}
         title='Funkcamp at Åsafolkhögkola'
         desc='Tony Gogo visiting åsa folkhögskola 2022. In 2022 we decided to go visit the school in the woods and give the studens a chance to learn from a pioneer. '
         tag='Camp'
