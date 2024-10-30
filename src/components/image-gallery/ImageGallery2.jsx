873f90242a35bac33c0ea2a4c056b8f6b55f2ae8
FunkSwe <funkcampswe@gmail.com>
2024-10-30 08:57:49 +0100

    testing lazy load

--- src/components/image-gallery/ImageGallery2.jsx
+++ src/components/image-gallery/ImageGallery2.jsx
@@ -1,83 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, Suspense } from 'react';
 import {
   RiCloseFill,
   RiArrowRightCircleLine,
   RiArrowLeftCircleLine,
 } from 'react-icons/ri';
-import Img_161 from '/fc16/1.jpg';
-import Img_162 from '/fc16/2.jpg';
-import Img_163 from '/fc16/3.jpg';
-import Img_164 from '/fc16/4.jpg';
-import Img_165 from '/fc16/5.jpg';
-import Img_166 from '/fc16/6.jpg';
-import Img_181 from '/fc18/1.jpg';
-import Img_182 from '/fc18/2.jpg';
-import Img_183 from '/fc18/3.jpg';
-import Img_186 from '/fc18/6.jpg';
-import Img_187 from '/fc18/7.jpg';
-import Img_188 from '/fc18/8.jpg';
-import Img_189 from '/fc18/9.jpg';
-import Img_1810 from '/fc18/10.jpg';
-import Img_1811 from '/fc18/11.jpg';
-import Img_1812 from '/fc18/12.jpg';
-import Img_1813 from '/fc18/13.jpg';
-import Img_1814 from '/fc18/14.jpg';
-import Img_231 from '/fc23/1.jpg';
-import Img_232 from '/fc23/2.jpg';
-import Img_233 from '/fc23/4.jpg';
-import Img_241 from '/fc24/1.jpg';
-import Img_242 from '/fc24/1.jpg';
-import Img_243 from '/fc24/1.jpg';
-import Img_244 from '/fc24/1.jpg';
-import Img_245 from '/fc24/1.jpg';
-import Img_246 from '/fc24/6.jpg';
-import Img_247 from '/fc24/7.jpg';
-import { Img } from 'react-image';
 import { motion, AnimatePresence } from 'framer-motion';
-
-// Random images from Pexels
-/* const randomImages = [
-  'https://images.pexels.com/photos/34950/pexels-photo.jpg',
-  'https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg',
-  'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg',
-]; */
+import { Img } from 'react-image';
+import LazyLoad from 'react-lazyload';
 
 const images = [
-  Img_161,
-  Img_162,
-  Img_163,
-  Img_164,
-  Img_165,
-  Img_166,
-  Img_181,
-  Img_182,
-  Img_183,
-  Img_186,
-  Img_187,
-  Img_188,
-  Img_189,
-  Img_1810,
-  Img_1811,
-  Img_1812,
-  Img_1813,
-  Img_1814,
-  Img_231,
-  Img_232,
-  Img_233,
-  Img_241,
-  Img_242,
-  Img_243,
-  Img_244,
-  Img_245,
-  Img_246,
-  Img_247,
-  /*  ...randomImages, */
+  '/fc16/1.jpg', '/fc16/2.jpg', '/fc16/3.jpg', '/fc16/4.jpg', '/fc16/5.jpg', '/fc16/6.jpg',
+  '/fc18/1.jpg', '/fc18/2.jpg', '/fc18/3.jpg', '/fc18/6.jpg', '/fc18/7.jpg', '/fc18/8.jpg',
+  '/fc18/9.jpg', '/fc18/10.jpg', '/fc18/11.jpg', '/fc18/12.jpg', '/fc18/13.jpg', '/fc18/14.jpg',
+  '/fc23/1.jpg', '/fc23/2.jpg', '/fc23/4.jpg', '/fc24/1.jpg', '/fc24/2.jpg', '/fc24/3.jpg',
+  '/fc24/4.jpg', '/fc24/5.jpg', '/fc24/6.jpg', '/fc24/7.jpg'
 ];
 
 const ImageGallery2 = () => {
   const [selectedIndex, setSelectedIndex] = useState(null);
 
-  const openLightbox = (index) => {
+  const openLightbox = async (index) => {
     setSelectedIndex(index);
   };
 
@@ -87,32 +29,30 @@ const ImageGallery2 = () => {
 
   const showPrev = (e) => {
     e.stopPropagation();
-    setSelectedIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
+    setSelectedIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
   };
 
   const showNext = (e) => {
     e.stopPropagation();
-    setSelectedIndex((prevIndex) =>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1
-    );
+    setSelectedIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
   };
 
   return (
-    <div className='p-8 md:p-40'>
+    <div className="p-8 md:p-40">
       <p>I want more images... ok!</p>
-      <div className='columns-2 sm:columns-2 md:columns-3 md:gap-8'>
+      <div className="columns-2 sm:columns-2 md:columns-3 md:gap-8">
         {images.map((image, index) => (
-          <div key={index} className='mb-8 break-inside-avoid'>
-            <Img
-              className='w-full cursor-pointer rounded-sm'
-              alt={`Gallery ${index}`}
-              src={image}
-              loader={<div>Loading...</div>}
-              unloader={<div>Failed to load image</div>}
-              onClick={() => openLightbox(index)}
-            />
+          <div key={index} className="mb-8 break-inside-avoid">
+            <LazyLoad height={200} offset={100} once>
+              <Img
+                className="w-full cursor-pointer rounded-sm"
+                alt={`Gallery ${index}`}
+                src={image}
+                loader={<div>Loading...</div>}
+                unloader={<div>Failed to load image</div>}
+                onClick={() => openLightbox(index)}
+              />
+            </LazyLoad>
           </div>
         ))}
       </div>
@@ -120,14 +60,14 @@ const ImageGallery2 = () => {
       <AnimatePresence>
         {selectedIndex !== null && (
           <motion.div
-            className='fixed inset-0 bg-black flex items-center justify-center z-50'
+            className="fixed inset-0 bg-black flex items-center justify-center z-50"
             onClick={closeLightbox}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
             <motion.div
-              className='fixed inset-0 z-0'
+              className="fixed inset-0 z-0"
               style={{
                 backgroundImage: `url(${images[selectedIndex]})`,
                 backgroundSize: 'cover',
@@ -139,7 +79,7 @@ const ImageGallery2 = () => {
 
             {/* Close Button */}
             <button
-              className='absolute bottom-4 right-8 text-white text-xl'
+              className="absolute bottom-4 right-8 text-white text-xl"
               onClick={closeLightbox}
             >
               <RiCloseFill />
@@ -147,7 +87,7 @@ const ImageGallery2 = () => {
 
             {/* Previous Button */}
             <button
-              className='absolute left-4 text-white text-3xl'
+              className="absolute left-4 text-white text-3xl"
               onClick={showPrev}
             >
               <RiArrowLeftCircleLine />
@@ -155,7 +95,7 @@ const ImageGallery2 = () => {
 
             {/* Next Button */}
             <button
-              className='absolute right-4 text-white text-3xl'
+              className="absolute right-4 text-white text-3xl"
               onClick={showNext}
             >
               <RiArrowRightCircleLine />
@@ -168,13 +108,15 @@ const ImageGallery2 = () => {
               exit={{ opacity: 0, scale: 0.9 }}
               transition={{ duration: 0.4 }}
             >
-              <Img
-                src={images[selectedIndex]}
-                alt={`Gallery ${selectedIndex}`}
-                className='max-w-[80%] md:max-w-[60%] max-h-auto shadow-lg z-10 mx-auto'
-                loader={<div>Loading...</div>}
-                unloader={<div>Failed to load image</div>}
-              />
+              <Suspense fallback={<div>Loading...</div>}>
+                <Img
+                  src={images[selectedIndex]}
+                  alt={`Gallery ${selectedIndex}`}
+                  className="max-w-[80%] md:max-w-[60%] max-h-auto shadow-lg z-10 mx-auto"
+                  loader={<div>Loading...</div>}
+                  unloader={<div>Failed to load image</div>}
+                />
+              </Suspense>
             </motion.div>
           </motion.div>
         )}
